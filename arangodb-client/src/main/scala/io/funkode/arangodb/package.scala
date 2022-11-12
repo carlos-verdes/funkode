1a4180122915d18117f60a7009c919fc4aad945d
Carlos Verdes <cverdes@gmail.com>
2022-11-12 23:09:47 +0400

    simplify arango api

--- arangodb-client/src/main/scala/io/funkode/arangodb/package.scala
+++ arangodb-client/src/main/scala/io/funkode/arangodb/package.scala
@@ -9,6 +9,7 @@ import protocol.*
 
 type AIO[A] = IO[ArangoError, A]
 type RAIO[Encoder[_], Decoder[_], O] = ZIO[ArangoClient[Encoder, Decoder], ArangoError, O]
+type WithApi[Encoder[_], Decoder[_], O] = ZIO[ArangoApi[Encoder, Decoder], Nothing, O]
 
 val Api = "_api"
 val Collection = "collection"
